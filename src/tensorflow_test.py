7cde63aa3de59248987b7e8fd7cfcc9df1bf13ab
Paul Easter <paul.john.easter@gmail.com>
2023-12-27 23:39:17 +1100

    Test program added

--- src/tensorflow_test.py
+++ src/tensorflow_test.py
@@ -1,2 +1,42 @@
 import tensorflow as tf
 print("TensorFlow version:", tf.__version__)
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
+
+predictions = model(x_train[:1]).numpy()
+print("Predictions:", predictions)
+
+softmax_predictions = tf.nn.softmax(predictions).numpy()
+print("Softmax Predictions:", softmax_predictions)
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+loss_value = loss_fn(y_train[:1], predictions).numpy()
+print("Loss Value:", loss_value)
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+evaluation = model.evaluate(x_test,  y_test, verbose=2)
+print("Model Evaluation:", evaluation)
+
+probability_model = tf.keras.Sequential([
+  model,
+  tf.keras.layers.Softmax()
+])
+
+probabilities = probability_model(x_test[:5])
+print("Probabilities for First 5 Test Samples:", probabilities)
